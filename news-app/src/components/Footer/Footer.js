17ff8815cab7f1e0124cce89862868e8496b2f8d
Piyush Dhakar <71017512+piyushdhakar@users.noreply.github.com>
2022-01-05 23:14:23 +0530

    Update Footer.js

--- news-app/src/components/Footer/Footer.js
+++ news-app/src/components/Footer/Footer.js
@@ -6,22 +6,10 @@ const Footer = () => {
     <div className="footer">
       <span className="name">
         Inshorts clone made by -{" "}
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
-          Piyush Agarwal
-        </a>
+        
+          Piyush Dhakar
+        
       </span>
-      <hr style={{ width: "90%" }} />
-      <div className="iconContainer">
-        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
-          <i className="fab fa-instagram-square fa-2x"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
-          <i className="fab fa-linkedin fa-2x"></i>
-        </a>
-        <a href="https://piyushjsx.netlify.app/" target="__blank">
-          <i className="fas fa-link fa-2x"></i>
-        </a>
-      </div>
     </div>
   );
 };
